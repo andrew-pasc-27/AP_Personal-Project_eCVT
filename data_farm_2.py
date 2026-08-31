1a3d78ae338c357be16a8c734816915e15c7cb87
andpasc75-alt <andpasc75@gmail.com>
2026-08-30 21:27:03 -0700

    Update data_farm_2.py

--- data_farm_2.py
+++ data_farm_2.py
@@ -3,14 +3,15 @@ from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C,
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import ColorSensor, UltrasonicSensor
 import time
-
+#initialize motors
 motor_a = MediumMotor(OUTPUT_A)
 motor_b = LargeMotor(OUTPUT_B)
 motor_c = LargeMotor(OUTPUT_C)
 
 results = []
 m = [motor_a, motor_b, motor_c]
-
+#previously used another outer loop to loop through motors
+#also allows for simple changing of motor depending on which test you want to run
 for j in range(10, 101, 10):
     m[2].stop(stop_action="coast")
     m[0].on(speed=-j)
@@ -24,7 +25,7 @@ for j in range(10, 101, 10):
 motor_a.off()
 motor_b.off()
 motor_c.off()
-
+#for copy and pasting into data_analysis.py
 print(results)
 
 print("Done!")
